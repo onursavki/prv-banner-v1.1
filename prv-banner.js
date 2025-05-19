c31dbf5aecda0278ec950ac33ee416437f83ca96
Onur Şavkı <42342278+onursavki@users.noreply.github.com>
2025-05-20 00:33:27 +0300

    prv-banner.js

--- prv-banner.js
+++ prv-banner.js
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     else if (today >= banner6StartDate && today <= banner6EndDate) {
         document.getElementById('banner6').style.display = 'block';
-        document.getElementById('banner-container').style.background = `linear-gradient(to top, #ea2421, #dd1106, #d40701), linear-gradient(to right, rgba(255,255,255,0) 50%, #ffffff 50%)`;
+        document.getElementById('banner-container').style.background = `linear-gradient(to right, #ffffff 50%, transparent 50%), linear-gradient(to top, #ea2421, #dd1106, #d40701)`;
 
     }
     else if (today >= banner7StartDate && today <= banner7EndDate) {
